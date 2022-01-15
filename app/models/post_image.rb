4d6d828d9748e9e48953bacf3d3775bb6a12e861
soccercreater <96107026+soccercreater@users.noreply.github.com>
2022-01-15 13:57:49 +0000

    19finish

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,6 +2,7 @@ class PostImage < ApplicationRecord
   has_one_attached :image
 belongs_to :user
 has_many :post_comments, dependent: :destroy
+has_many :favorites, dependent: :destroy
 
   def get_image
     unless image.attached?
@@ -11,6 +12,9 @@ has_many :post_comments, dependent: :destroy
     image
   end
 
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
 
 
 end
